48cd8f5581f92d02a2e7a927217af19ca16ed68f
Viktor <ViktorYu@web.de>
2021-11-19 19:42:53 +0100

    Add mod files

---
+++ modinfo.lua
@@ -0,0 +1,188 @@
+name = "Burning Timer"
+forumthread = ""
+description = [[Displays a timer above burning objects or any campfire telling you the time until the fire goes out.]]
+author = "Viktor"
+version = "1.1.3"
+api_version = 10
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+dst_compatible = true
+all_clients_require_mod = false
+client_only_mod = true
+server_filter_tags = {}
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+local Keys = {
+	{description = "None --", data = -1}, -- If players can't find the None option
+	{description = "Numpad Zero", data = 256},
+	{description = "Numpad One", data = 257},
+	{description = "Numpad Two", data = 258},
+	{description = "Numpad Three", data = 259},
+	{description = "Numpad Four", data = 260},
+	{description = "Numpad Five", data = 261},
+	{description = "Numpad Six", data = 262},
+	{description = "Numpad Seven", data = 263},
+	{description = "Numpad Eight", data = 264},
+	{description = "Numpad Nine", data = 265},
+	{description = "Numpad Period", data = 266},
+	{description = "Numpad Divide", data = 267},
+	{description = "Numpad Multiply", data = 268},
+	{description = "Numpad Minus", data = 269},
+	{description = "Numpad Plus", data = 270},
+--	{description = "Numpad Enter", data = 271},
+--	{description = "Numpad Equals", data = 272},
+	{description = "Up", data = 273},
+	{description = "Down", data = 274},
+	{description = "Left", data = 276},
+	{description = "Right", data = 275},
+	{description = "Minus", data = 45},
+	{description = "Plus", data = 43},
+	{description = "LeftBracket", data = 91},
+	{description = "RightBracket", data = 93},
+	{description = "Semicolon", data = 59},
+	{description = "Quote", data = 39},
+	{description = "Comma", data = 44},
+	{description = "Period", data = 46},
+	{description = "Slash", data = 47},
+	{description = "PageUp", data = 280},
+	{description = "PageDown", data = 281},
+	{description = "Home", data = 278},
+	{description = "End", data = 279},
+	{description = "Insert", data = 277},
+	{description = "Delete", data = 127},
+	{description = "-- None --", data = 0}, -- Placing none here could help others to set up their keys
+	{description = "A", data = 97},
+	{description = "B", data = 98},
+	{description = "C", data = 99},
+	{description = "D", data = 100},
+	{description = "E", data = 101},
+	{description = "F", data = 102},
+	{description = "G", data = 103},
+	{description = "H", data = 104},
+	{description = "I", data = 105},
+	{description = "J", data = 106},
+	{description = "K", data = 107},
+	{description = "L", data = 108},
+	{description = "M", data = 109},
+	{description = "N", data = 110},
+	{description = "O", data = 111},
+	{description = "P", data = 112},
+	{description = "Q", data = 113},
+	{description = "R", data = 114},
+	{description = "S", data = 115},
+	{description = "T", data = 116},
+	{description = "U", data = 117},
+	{description = "V", data = 118},
+	{description = "W", data = 119},
+	{description = "X", data = 120},
+	{description = "Y", data = 121},
+	{description = "Z", data = 122},
+	{description = "F1", data = 282},
+	{description = "F2", data = 283},
+	{description = "F3", data = 284},
+	{description = "F4", data = 285},
+	{description = "F5", data = 286},
+	{description = "F6", data = 287},
+	{description = "F7", data = 288},
+	{description = "F8", data = 289},
+	{description = "F9", data = 290},
+	{description = "F10", data = 291},
+	{description = "F11", data = 292},
+	{description = "F12", data = 293},
+	{description = "-- None", data = -1}, -- To avoid forcing players to click all the way back to the beginning
+}
+local Boolean = {
+	{description = "Disabled", data = false},
+	{description = "Enabled", data = true},
+}
+local BooleanHidden = {
+	{description = "Disabled", data = false},
+	{description = "Hidden", data = "hidden", hover = "Timer will only show up if you hover above it with your cursor."},
+	{description = "Enabled", data = true},
+}
+
+configuration_options = {
+{
+	name = "hideButton",
+	label = "Show/Hide Burning Timer",
+	hover = "Toggles the visibility of the burning timers if pressed.",
+	options = Keys,
+	default = 0,
+},
+{
+	name = "enabledByDefault",
+	label = "Enabled by default",
+	hover = "Enables/Disables the visibility of the burning timers at the start of your game.",
+	options = Boolean,
+	default = true,
+},
+{
+	name = "",
+	label = "",
+	hover = "",
+	options = {{description = "", data = 0}},
+	default = 0,
+},
+{
+	name = "showBurningTimer",
+	label = "Show Burning Timer",
+	hover = "Burning objects will show the remaining time until they burn down to ashes.",
+	options = Boolean,
+	default = true,
+},
+{
+	name = "showCampfireTimer",
+	label = "Show Campfire Timer",
+	hover = "Fire Pits, Campfires, Night Lights, etc.\nwill show their fuel and the remaining time until the fire goes out.",
+	options = BooleanHidden,
+	default = true,
+},
+{
+	name = "showLanternTimer",
+	label = "Show Lantern Timer",
+	hover = "Lanterns will show their fuel and the remaining time until they turn off.",
+	options = BooleanHidden,
+	default = true,
+},
+{
+	name = "showStarTimer",
+	label = "Show Star Caller Timer",
+	hover = "Dwarf Stars and Polar Lights will show their remaining time in days and in minutes until they disappear.",
+	options = BooleanHidden,
+	default = true,
+},
+{
+	name = "showHiddenDuration",
+	label = "Unhide Duration",
+	hover = "How long the timers will be shown if set to 'Hidden'.",
+	options = {
+		{description = "0.5s", data = 0.5},
+		{description =  "1s", data = 1.0},
+		{description =  "2s", data = 2.0},
+		{description =  "3s", data = 3.0},
+		{description =  "4s", data = 4.0},
+		{description =  "5s", data = 5.0},
+		{description =  "6s", data = 6.0},
+		{description =  "7s", data = 7.0},
+		{description =  "8s", data = 8.0},
+		{description =  "9s", data = 9.0},
+		{description = "10s", data = 10.0},
+		{description = "15s", data = 15.0},
+		{description = "20s", data = 20.0},
+		{description = "25s", data = 25.0},
+		{description = "30s", data = 30.0},
+		{description = "45s", data = 45.0},
+		{description =  "1m", data = 60.0},
+		{description =  "2m", data = 120.0},
+		{description =  "5m", data = 300.0},
+		{description = "10m", data = 600.0},
+		{description = "15m", data = 900.0},
+		{description = "20m", data = 1200.0},
+		{description = "30m", data = 1800.0},
+		{description =  "1h", data = 3600.0},
+		{description = "3.14h", data = 11309.74, hover = "I doubt you can keep a fire that long without hovering above it."},
+	},
+	default = 5.0,
+},
+} -- The End
---
+++ modmain.lua
@@ -0,0 +1,307 @@
+--[[ Burning Timer ]]--
+
+-- Known Issues:
+-- Timers will become incorrect if the server is lagging (No way to correct that bug since the lag is only server sided, not client sided)
+-- Tree Stumps show incorrect burning timers (Displays as 45s but is actually 10s)
+-- Timers will be incorrect if the object is already burning before being loaded (In this case, objects will be marked via brackets)
+
+-- For testing:
+-- AoE-Burning Spell:
+--	pos = ThePlayer:GetPosition() for k,v in pairs(TheSim:FindEntities(pos.x,0,pos.z,64)) do if v.components and v.components.burnable then v.components.burnable:Ignite() end end
+
+local _G = GLOBAL
+local TUNING = _G.TUNING
+
+_G.mod_burningTimer = {}
+_G.mod_burningTimer.enabled = GetModConfigData("enabledByDefault")
+_G.mod_burningTimer.burntimeList = { -- To avoid crashes, certain burntimers have to be hardcoded
+	blueprint = TUNING.SMALL_BURNTIME,
+}
+_G.mod_burningTimer.burntimeRNG = {
+--	blueprint = nil,
+}
+_G.mod_burningTimer.campfireMaxFuel = {}
+_G.mod_burningTimer.campfireFuelRate = {}
+_G.mod_burningTimer.campfireRainRate = {}
+_G.mod_burningTimer.campfireFireLevels = {}
+
+_G.mod_burningTimer.lanternLightTime = {}
+_G.mod_burningTimer.lanternIntensityMin = {}
+_G.mod_burningTimer.lanternIntensityDiff = {}
+
+_G.mod_burningTimer.campfireReveal = GetModConfigData("showCampfireTimer") == "hidden" and 0.0 or nil
+_G.mod_burningTimer.lanternReveal = GetModConfigData("showLanternTimer") == "hidden" and 0.0 or nil
+_G.mod_burningTimer.starReveal = GetModConfigData("showStarTimer") == "hidden" and 0.0 or nil
+_G.mod_burningTimer.revealDuration = GetModConfigData("showHiddenDuration")
+
+_G.mod_burningTimer.burntimeTextSize = 30
+_G.mod_burningTimer.campfireTextSize = 30
+_G.mod_burningTimer.lanternTextSize = 30
+_G.mod_burningTimer.starTextSize = 30
+
+_G.mod_burningTimer.fetchingTimer = false
+_G.mod_burningTimer.debug = false
+
+_G.mod_burningTimer.validFueltypes = {}
+for k, v in pairs(_G.FUELTYPE) do
+	if v ~= _G.FUELTYPE.USAGE then -- Not a real fuel, according to Klei's code
+		table.insert(_G.mod_burningTimer.validFueltypes, v.."_fueled")
+	end
+end
+
+local burntimeList         = _G.mod_burningTimer.burntimeList
+local burntimeRNG          = _G.mod_burningTimer.burntimeRNG
+local campfireMaxFuel      = _G.mod_burningTimer.campfireMaxFuel
+local campfireFuelRate     = _G.mod_burningTimer.campfireFuelRate
+local campfireRainRate     = _G.mod_burningTimer.campfireRainRate
+local campfireFireLevels   = _G.mod_burningTimer.campfireFireLevels
+local lanternLightTime     = _G.mod_burningTimer.lanternLightTime
+local lanternIntensityMin  = _G.mod_burningTimer.lanternIntensityMin
+local lanternIntensityDiff = _G.mod_burningTimer.lanternIntensityDiff
+
+-- Copied from 'Item Info' mod made by 'Ryuu' to fix a bug which may happen in some cases, credit goes to Ryuu
+AddGlobalClassPostConstruct("entityscript","EntityScript", function(self)
+	local oldRegisterComponentActions = self.RegisterComponentActions
+
+	self.RegisterComponentActions = function(self, name)
+		if not _G.mod_burningTimer.fetchingTimer then
+			return oldRegisterComponentActions(self, name)
+		end
+	end
+end) 
+
+-- The way how to fetch burning timers is based on the 'Item Info' mod made by 'Ryuu', thanks to Ryuu
+local function fetchBurntime(prefab)
+	if burntimeList[prefab] then return false end
+
+	local inst
+	local IsMasterSim = _G.TheWorld.ismastersim
+	_G.TheWorld.ismastersim = true -- Your ingame data has just been corrupted
+	_G.mod_burningTimer.fetchingTimer = true -- Fetching Burning Timers has been enabled
+	local math_random_orig = math.random -- Your RNG is going to be corrupted
+
+	math.random = function(a,b) return a and b and a or a and 1 or 0.0 end -- Set random to lowest value possible
+	inst = _G.SpawnPrefab(prefab)
+	if inst and inst.components.burnable and inst.components.burnable.burntime then
+		burntimeList[prefab] = inst.components.burnable.burntime
+	else
+		burntimeList[prefab] = false
+	end
+	inst:Remove()
+
+	if burntimeList[prefab] then -- boats do technically burn forever
+		math.random = function(a,b) return a and b and b or a and a or 1.0 end -- Set random to highest value possible
+		inst = _G.SpawnPrefab(prefab)
+		if inst and inst.components.burnable and inst.components.burnable.burntime then
+			burntimeRNG[prefab] = inst.components.burnable.burntime - burntimeList[prefab]
+		end
+		if burntimeRNG[prefab] == 0.0 then
+			burntimeRNG[prefab] = nil
+		end
+		inst:Remove()
+	end
+
+	math.random = math_random_orig -- Your RNG is fine again
+	_G.mod_burningTimer.fetchingTimer = false -- Fetching Burning Timers has been disabled
+	_G.TheWorld.ismastersim = IsMasterSim -- Your ingame data is fine again
+
+	if _G.mod_burningTimer.debug then
+		print("Fetched Burning Stats from",prefab,"- Burntime:",burntimeList[prefab],"Burntime RNG:",burntimeRNG[prefab])
+	end
+	return true
+end
+
+local function fetchCampfireStats(prefab)
+	if campfireMaxFuel[prefab] then return false end
+
+	local inst
+	local IsMasterSim = _G.TheWorld.ismastersim
+	_G.TheWorld.ismastersim = true -- Your ingame data has just been corrupted
+	_G.mod_burningTimer.fetchingTimer = true -- Fetching Burning Timers has been enabled
+
+	local IsRaining = _G.TheWorld.state.israining -- Your rain is going to be corrupted
+	local PrecipitationRate = _G.TheWorld.state.precipitationrate
+
+	_G.TheWorld.state.israining = false -- Rain suddenly stopped.
+	inst = _G.SpawnPrefab(prefab)
+	if inst and inst.components.fueled and inst.components.fueled.rate then
+		campfireMaxFuel[prefab]    = inst.components.fueled.maxfuel
+		campfireFuelRate[prefab]   = inst.components.fueled.rate
+		campfireFireLevels[prefab] = 
+			inst.components.burnable
+			and inst.components.burnable.fxchildren[1]
+			and inst.components.burnable.fxchildren[1].components
+			and inst.components.burnable.fxchildren[1].components.firefx
+			and inst.components.burnable.fxchildren[1].components.firefx.levels
+			or {}
+	else
+		campfireMaxFuel[prefab] = false
+	end
+	inst:Remove()
+
+	if campfireMaxFuel[prefab] then
+		local toSave
+		_G.TheWorld.state.israining = true -- Let there be rain!
+		_G.TheWorld.state.precipitationrate = 0.0 -- But then again, there's no rain rn
+		inst = _G.SpawnPrefab(prefab)
+		toSave = inst.components.fueled.rate
+		inst:Remove()
+		_G.TheWorld.state.precipitationrate = 1.0 -- Suddenly rain appeared
+		inst = _G.SpawnPrefab(prefab)
+		campfireRainRate[prefab] = inst.components.fueled.rate - toSave
+		inst:Remove()
+	end
+
+	_G.TheWorld.state.israining = IsRaining -- Your rain is fine again
+	_G.TheWorld.state.precipitationrate = PrecipitationRate -- Your precipitation rate is fine again
+	_G.mod_burningTimer.fetchingTimer = false -- Fetching Burning Timers has been disabled
+	_G.TheWorld.ismastersim = IsMasterSim -- Your ingame data is fine again
+
+	if _G.mod_burningTimer.debug then
+		print("Fetched Campfire Stats from",prefab,"- Max Fuel:",campfireMaxFuel[prefab],"Fuel Rate:",campfireFuelRate[prefab],"Rain Rate:",campfireRainRate[prefab],"Fire Levels:",campfireFireLevels[prefab])
+	end
+	return true
+end
+
+-- I know there's only one lantern ingame which has constant stats
+-- ... but there might also be other lanterns nearby, or new lanterns, or lanterns with different stats
+-- That's why we anyway fetch the lantern stats
+local function fetchLanternStats(prefab)
+	if lanternLightTime[prefab] then return false end
+
+	local inst
+	local IsMasterSim = _G.TheWorld.ismastersim
+	_G.TheWorld.ismastersim = true -- Your ingame data has just been corrupted
+	_G.mod_burningTimer.fetchingTimer = true -- Fetching Burning Timers has been enabled
+
+	inst = _G.SpawnPrefab(prefab)
+	if inst and inst.components.fueled and (inst.components.fueled.updatefn or inst.components.fueled.ontakefuelfn) then
+		local updatefn = inst.components.fueled.updatefn or inst.components.fueled.ontakefuelfn
+		lanternLightTime[prefab] = inst.components.fueled.maxfuel
+		if inst.components.machine and inst.components.machine.turnonfn then
+			inst.components.machine.turnonfn(inst)
+		elseif inst.components.equippable and inst.components.equippable.onequipfn then
+			inst.components.equippable.onequipfn(inst, _G.ThePlayer, true)
+		end
+		local _light = inst._light
+		if _light then
+			inst.components.fueled.currentfuel = inst.components.fueled.maxfuel
+			updatefn(inst)
+			local maxLight = _light.Light:GetRadius()
+			inst.components.fueled.currentfuel = 0.0
+			updatefn(inst)
+			lanternIntensityMin[prefab] = _light.Light:GetRadius()
+			lanternIntensityDiff[prefab] = maxLight - lanternIntensityMin[prefab]
+			if lanternIntensityDiff[prefab] == 0.0 then
+				lanternLightTime[prefab] = false
+			end
+		else
+			lanternLightTime[prefab] = false
+		end
+	else
+		lanternLightTime[prefab] = false
+	end
+	inst:Remove()
+
+	_G.mod_burningTimer.fetchingTimer = false -- Fetching Burning Timers has been disabled
+	_G.TheWorld.ismastersim = IsMasterSim -- Your ingame data is fine again
+
+	if _G.mod_burningTimer.debug then
+		print("Fetched Lantern Stats from",prefab,"- Light Time:",lanternLightTime[prefab],"Intensity Min:",lanternIntensityMin[prefab],"Intensity Diff:",lanternIntensityDiff[prefab])
+	end
+	return true
+end
+
+local function getBurntime(prefab)
+	if not prefab then return 0.0, nil end
+	if burntimeList[prefab] == nil then fetchBurntime(prefab) end
+	if not burntimeList[prefab] then return 0.0, nil end
+	return burntimeList[prefab], burntimeRNG[prefab]
+end
+
+local function getCampfireStats(prefab)
+	if not prefab then return false, 0.0, 0.0, {} end
+	if campfireMaxFuel[prefab] == nil then fetchCampfireStats(prefab) end
+	if not campfireMaxFuel[prefab] then return false, 0.0, 0.0, {} end
+	return campfireMaxFuel[prefab], campfireFuelRate[prefab], campfireRainRate[prefab], campfireFireLevels[prefab]
+end
+
+local function getLanternStats(prefab)
+	if not prefab then return false, 0.0, 0.0 end
+	if lanternLightTime[prefab] == nil then fetchLanternStats(prefab) end
+	if not lanternLightTime[prefab] then return false, 0.0, 0.0 end
+	return lanternLightTime[prefab], lanternIntensityMin[prefab], lanternIntensityDiff[prefab]
+end
+
+_G.mod_burningTimer.getBurntime = getBurntime
+_G.mod_burningTimer.getCampfireStats = getCampfireStats
+_G.mod_burningTimer.getLanternStats = getLanternStats
+
+local function InGame() return _G.ThePlayer and _G.ThePlayer.HUD and not _G.ThePlayer.HUD:HasInputFocus() end
+if GetModConfigData("hideButton") and GetModConfigData("hideButton") > 0 then
+	_G.TheInput:AddKeyDownHandler(GetModConfigData("hideButton"),function()
+		if InGame() and _G.TheWorld and _G.mod_burningTimer then
+			_G.mod_burningTimer.enabled = not _G.mod_burningTimer.enabled
+			if _G.ThePlayer and _G.ThePlayer.components and _G.ThePlayer.components.talker then
+				_G.ThePlayer.components.talker:Say("Burning Timer: "..(_G.mod_burningTimer.enabled and "Enabled" or "Disabled"))
+			end
+		end
+	end)
+end
+
+-- Burning Timer --
+if GetModConfigData("showBurningTimer") then
+	AddPrefabPostInit("fire", function(inst)
+		inst:DoTaskInTime(0.01, function()
+			inst:AddComponent("burningtimer")
+		end)
+		return
+	end)
+end
+
+-- Campfire Timer --
+local function campfireTimer(inst)
+	inst:DoTaskInTime(0.1, function()
+		inst:AddComponent("campfiretimer")
+	end)
+	return
+end
+if GetModConfigData("showCampfireTimer") then
+	AddPrefabPostInit("campfirefire",     function(inst) return campfireTimer(inst) end)
+	AddPrefabPostInit("coldfirefire",     function(inst) return campfireTimer(inst) end)
+	AddPrefabPostInit("nightlight_flame", function(inst) return campfireTimer(inst) end)
+	--AddPrefabPostInit("pigtorch_flame", function(inst) return campfireTimer(inst) end) -- It works for pig torches but their light range does not depend on their available fuel so this mod will show obviously invalid numbers
+	AddPrefabPostInit("obsidianfirefire", function(inst) return campfireTimer(inst) end) -- From Tropical Experience mod
+end
+
+-- Lantern Timer --
+local function lanternTimer(inst)
+	inst:DoTaskInTime(0.01, function()
+		inst:AddComponent("lanterntimer")
+	end)
+	return
+end
+if GetModConfigData("showLanternTimer") then
+	AddPrefabPostInit("lanternlight",     function(inst) return lanternTimer(inst) end)
+end
+
+-- Star Timer --
+local function starTimer(inst)
+	inst:DoTaskInTime(0.01, function()
+		inst:AddComponent("startimer")
+	end)
+	return
+end
+if GetModConfigData("showStarTimer") then
+	AddPrefabPostInit("stafflight",       function(inst) return starTimer(inst) end)
+	AddPrefabPostInit("staffcoldlight",   function(inst) return starTimer(inst) end)
+end
+
+if GetModConfigData("showCampfireTimer") == "hidden" or GetModConfigData("showLanternTimer") == "hidden" or GetModConfigData("showStarTimer") == "hidden" then
+	AddPlayerPostInit(function(inst)
+		inst:DoTaskInTime(1, function()
+			if inst ~= _G.ThePlayer then return end
+			inst:AddComponent("bt_revealer")
+		end)
+	end)
+end
